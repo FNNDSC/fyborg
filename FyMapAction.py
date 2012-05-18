699307782bab8080246fad0193e51daf5fc9d22b
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-05-18 09:32:28 -0400

    Add skeleton to perform spline based interpolation for scalar lookup in FyMapAction.

--- FyMapAction.py
+++ FyMapAction.py
@@ -14,12 +14,39 @@ class FyMapAction( FyAction ):
     self._imageDimensions = self._image.shape[:3]
     self._imageSpacing = self._imageHeader.get_zooms()
 
+    self._previous = {}
+
   def scalarPerCoordinate( self, uniqueFiberId, x, y, z ):
     '''
     '''
+
+    # check if we already have a list of previous points for this fiber id
+    # if not, create one
+    # if yes, attach the current point (append)
+
+    # if we have a previous 
+
+    if not uniqueFiberId in self._previous:
+      # we don't have a list
+      self._previous[uniqueFiberId] = []
+
+    # check if we have already a previous coordinate
+    if len( self._previous[uniqueFiberId] ) > 0:
+      # now do spline filtering using last element of the coordinate list and the current one
+      lastCoord = self._previous[uniqueFiberId][-1]
+
+    # todo spline creation and lookup scalar values in respect to spacing self._imageSpacing
+    # and recalculate the value
+
+    # append the current coordinate
+    self._previous[uniqueFiberId].append( [x, y, z] )
+
+    # this will be obsolete
     ijkCoords = [x / y for x, y in zip( [x, y, z], self._imageSpacing )]
     ijkCoords = [max( 1, x ) for x in ijkCoords]
     value = self._image[int( ijkCoords[0] ), int( ijkCoords[1] ), int( ijkCoords[2] )]
 
+
+    # return the interpolated value
     return value
 
